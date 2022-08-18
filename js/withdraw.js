fca908193c8737ff60c4d58b9dc53e41b08b97cd
Rakibul Islam <rakibul241221@gmail.com>
2022-08-19 00:08:44 +0600

    deposit & others everything added

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,24 +1,31 @@
+// step 1 
 document.getElementById('withdraw-btn').addEventListener('click', function () {
 
+    // step 2 
     const withdrawInputElement = document.getElementById('withdraw-nput');
     const withdrawInputAmountString = withdrawInputElement.value;
     const withdrawInputAmount = parseFloat(withdrawInputAmountString);
 
+    // step 3 
     withdrawInputElement.value = '';
 
+    // step 4 
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const PreviousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
+    // step 5 
     const currentWithdrawTotal = PreviousWithdrawTotal + withdrawInputAmount;
 
+    // step 6 
     withdrawTotalElement.innerText = currentWithdrawTotal;
 
+    // step 7 
     const previousBalanceElement = document.getElementById('balance-total');
     const previousBalanceTotalString = previousBalanceElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
-
+    // step 8 
     const currentBalanceTotal = previousBalanceTotal - withdrawInputAmount;
     previousBalanceElement.innerText = currentBalanceTotal;
 })
